18bfbd295a7d27d08e048de71e50cba27421db3a
Krystof <krystofpavlis@gmail.com>
2022-10-13 21:19:04 +1100

    added UserList

---
+++ SimpleTextEditor/UserList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTextEditor
+{
+    internal class UserList
+    {
+        List<User> _users;
+
+        public UserList(List<User> users)
+        {
+            _users = users;
+        }
+
+        public void LoadUsers()
+        {
+            try
+            {
+                var file = File.ReadAllLines("login.txt");
+
+                foreach(var line in file)
+                {
+                    var accountData = new string[6];
+                    for (var i = 0; i < line.Length; i++) accountData[i] = line.Split(',')[i];
+
+                    var userName = accountData[0];
+                    var password = accountData[1];
+                    var type = accountData[2];
+                    var firstName = accountData[3];
+                    var lastName = accountData[4];
+                    var dob = DateTime.ParseExact(accountData[5], "dd-MM-yyyy", null);
+
+                    User newUser = new User(userName, password, type, firstName, lastName, dob);
+                    _users.Add(newUser);
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                throw;
+            }
+        }
+    }
+}
